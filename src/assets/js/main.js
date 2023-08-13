579afbd2b68ebe99ff7120a61b31caf4015d7f8d
Daniil Aristov <danilka-aristov@mail.ru>
2023-08-13 11:13:59 +0300

    create sleep part and unification travel and sleep block in holder

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,12 +7,14 @@ $(function(){
 		nextArrow: '<svg class="slider-arrows slider-arrows__right"><use xlink:href="assets/images/svg/sprite.svg#arrows-right"></use></svg>',
 		asNavFor: '.slider-dots-head',
 	})
+
 	$('.slider-dots-head').slick({
 		slidesToShow: 4,
 		slidesToScroll: 4,
 		swipe: false,
 		asNavFor: '.header__slider',
 	})
+
 	$('.surf-slider').slick({
 		slidesToShow: 4,
 		slidesToScroll: 1,
@@ -24,6 +26,7 @@ $(function(){
 		focusOnSelect: false,
 		asNavFor: '.slider-map',
 	})
+
 	$('.slider-map').slick({
 		slidesToShow: 8,
 		slidesToScroll: 1,
@@ -31,14 +34,36 @@ $(function(){
 		focusOnSelect: true,
 		asNavFor: '.surf-slider',
 	})
-	$('.travel__slider').slick({
+
+	$('.holder__slider').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		prevArrow: '<svg class="slider-arrows slider-arrows__left"><use xlink:href="assets/images/svg/sprite.svg#arrows-left"></use></svg>',
 		nextArrow: '<svg class="slider-arrows slider-arrows__right"><use xlink:href="assets/images/svg/sprite.svg#arrows-right"></use></svg>',
 		infinite: true,
 		fade: true,
-		autoplay: true,
-		autoplaySpeed: 3000,
+		autoplay: false,
 	})
+
+	$('<div class="quantity-button minus">-</div>').insertBefore('.quantity input[type="number"]');
+	$('<div class="quantity-button plus">+</div>').insertAfter('.quantity input[type="number"]');
+	$(document).ready(function(){
+		$('.plus').on('click', function() {
+				if ($(this).prev().val()) {
+						$(this).prev().val(+$(this).prev().val() + 1);
+				}
+		});
+		$('.minus').on('click', function() {
+				if ($(this).next().val() > 1) {
+						if ($(this).next().val() > 1) $(this).next().val(+$(this).next().val() - 1);
+				}
+		});
+	});
+
+	$('.quantity-button').on('click', function() {
+		let sum = Number($('.nights').val()) * Number($('.sum').data('nights')) + Number(($('.guests').val() - 1)) * $('.sum').data('guests');
+		$('.sum').html('$' + sum);
+	});
+	let sum = Number($('.nights').val()) * Number($('.sum').data('nights')) + Number(($('.guests').val() - 1)) * $('.sum').data('guests');
+	$('.sum').html('$' + sum);
 })
